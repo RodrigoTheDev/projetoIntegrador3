73ed2625c2315aa2ad581898b9662b7c1b893720
RodrigoTheDev <82519551+RodrigoTheDev@users.noreply.github.com>
2022-03-20 16:55:02 -0300

    Arquivos Adicionados

---
+++ tutorial.py
@@ -0,0 +1,10 @@
+#criando ambiente virtual:
+#"python -m vevn ambinete-virtual"
+
+#instalando bibliotecas:
+#digite no terminal após criar e se certificar que está dentro do ambiente virtual:
+#pip install pandas
+#pip install matplotlib
+#pip install notebook
+
+#também pode instalar pelo requirements.txt, mas aquelas bibliotecas todas são dependência dessa
